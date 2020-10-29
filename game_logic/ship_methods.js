0105cff4acad9ced686ed5779dc80e01bb9a3633
HassanKhan123 <hk2602809@gmail.com>
2020-10-29 07:46:04 +0500

    first test suite

---
+++ game_logic/ship_methods.js
@@ -0,0 +1,15 @@
+function checkForShip(player,coords){
+    let shipPresent,ship;
+
+    for(let i=0;i < player.ships.length;i++){
+        ship = player.ships[i]
+
+        shipPresent = ship.locations.filter((actualCoords) => {
+            return (actualCoords[0] === coords[0]) && (actualCoords[1] === coords[1])
+        })[0]
+
+        if(!shipPresent) return false
+    }
+}
+
+module.exports.checkForShip = checkForShip;
