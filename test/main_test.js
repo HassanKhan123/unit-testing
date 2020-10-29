c9c04e015f21f0e3dd297ea0a45121431b3885e1
HassanKhan123 <hk2602809@gmail.com>
2020-10-29 07:21:26 +0500

    test suite and test spec

---
+++ test/main_test.js
@@ -0,0 +1,10 @@
+let expect=require('chai').expect
+
+// test suite
+
+describe('Mocha',() => {
+    // test spec (unit test)
+    it('should run test using npm',() => {
+        expect(true).to.be.ok
+    })
+})
